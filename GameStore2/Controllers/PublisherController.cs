cf418161e76f709e74494891f62fc45437db9f09
nnifemi <odeyeminifemi@icloud.com>
2024-03-18 18:43:21 -0500

    PublisherController and View added.

---
+++ GameStore2/Controllers/PublisherController.cs
@@ -0,0 +1,26 @@
+﻿using BLL;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore2.Controllers
+{
+    public class PublisherController : Controller
+    {
+        // Instantiate the publisherService class
+        PublisherService publisherService = new PublisherService();
+
+        public IActionResult Index()
+        {
+            var response = publisherService.GetAllPublishersService();
+            return View(response);
+        }
+
+        [HttpPost]
+        public IActionResult AddPublisher([FromBody] Publisher publisherFormDataObject)
+        {
+            var response = publisherService.CreatePublisherService(publisherFormDataObject);
+            return Json(response);
+        }
+    }
+}
