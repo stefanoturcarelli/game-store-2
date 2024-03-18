75ff3e38b22c52fe2342c8a535d2e1fa07d6d969
Stefano <67341828+stefanoturcarelli@users.noreply.github.com>
2024-03-18 17:55:18 -0500

    Apply styles to home and products

--- GameStore2/Controllers/HomeController.cs
+++ GameStore2/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace GameStore2.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
