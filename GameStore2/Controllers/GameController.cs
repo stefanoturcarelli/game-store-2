6ada3adb5a3507ba55925943860728289b95272e
Stefano <67341828+stefanoturcarelli@users.noreply.github.com>
2024-03-18 21:49:49 -0500

    Implemented CRUD operations for Games

--- GameStore2/Controllers/GameController.cs
+++ GameStore2/Controllers/GameController.cs
@@ -12,15 +12,50 @@ namespace GameStore2.Controllers
 
         public IActionResult Index()
         {
-            var response = gameService.GetAllGamesService();
-            return View(response);
+            //var response = gameService.GetAllGamesService();
+            //return View(response);
+
+            return View();
+        }
+
+        // This method is called when the user clicks the "See Products" header link
+        [HttpGet]
+        public JsonResult ReadGames()
+        {
+            var products = gameService.GetAllGamesService();
+
+            return Json(products);
+        }
+
+        [HttpGet]
+        public JsonResult ReadGameById(int gameId)
+        {
+            var specificGame = gameService.GetGameByIdService(gameId);
+
+            return Json(specificGame);
         }
 
         [HttpPost]
-        public IActionResult AddGame([FromBody] Game gameObj)
+        public JsonResult AddGame([FromBody] Game gameObj)
         {
             var response = gameService.CreateGameService(gameObj);
             return Json(response);
+        }
+
+        [HttpPost]
+        public JsonResult UpdateGame([FromBody] Game gameToUpdate)
+        {
+            var gameEdited = gameService.EditGameService(gameToUpdate);
+            return Json(gameEdited);
+        }
+
+        [HttpPost]
+        public JsonResult DeleteGame([FromRoute] int id)
+        {
+            var gameDeleted = gameService.DeleteGameService(id);
+
+            return Json(gameDeleted);
+
         }
     }
 }
--- GameStore2/wwwroot/js/site.js
+++ GameStore2/wwwroot/js/site.js
@@ -3,9 +3,14 @@
 
 // Write your JavaScript code.
 
+$(document).ready(function () {
+    ReadGames();
+});
+
+
 function ReadGames() {
     $.ajax({
-        url: "/Game/ReadProductsController",
+        url: "/Game/ReadGames",
         type: "GET",
         contentType: "application/json",
         dataType: "json",
@@ -13,13 +18,13 @@ function ReadGames() {
             var html = '';
             $.each(response, function (key, item) {
                 html += '<tr>';
-                html += '<td>' + item.ProductName + '</td>';
-                html += '<td>' + item.ProductDescription + '</td>';
-                html += '<td>' + item.ProductPrice + '</td>';
-                html += '<td>' + item.ProductQuantity + '</td>';
-                html += '<td>' + item.ProductCategory + '</td>';
-                html += '<td>' + item.ProductSupplier + '</td>';
-                html += '<td><a class="record-action" href="#" onclick="ReadProductById(' + (item.ProductId || '') + ')">Edit</a> | <a class="record-action record-action__delete" href="#" onClick="DeleteProduct(' + (item.ProductId || '') + ')">Delete</a></td>';
+                html += '<td>' + item.publisherId + '</td>';
+                html += '<td>' + item.platformId + '</td>';
+                html += '<td>' + item.gameName + '</td>';
+                html += '<td>' + item.genreId + '</td>';
+                html += '<td>' + item.price + '</td>';
+                html += '<td>' + item.releaseDate + '</td>';
+                html += '<td><a class="record-action" href="#" onclick="ReadGameById(' + (item.gameId || '') + ')">Edit</a> | <a class="record-action record-action__delete" href="#" onClick="DeleteGame(' + (item.gameId || '') + ')">Delete</a></td>';
                 html += '</tr>';
             });
             $('.tbody').html(html);
@@ -29,3 +34,131 @@ function ReadGames() {
         }
     });
 }
+
+function ReadGameById(gameId) {
+    $.ajax({
+        url: "/Game/ReadGameById/" + gameId,
+        type: "GET",
+        data: { gameId: gameId },
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            $('#game-id').val(response.gameId);
+            $('#game-name').val(response.gameName);
+            $('#game-description').val(response.gameDescription);
+            $('#game-release-date').val(response.releaseDate);
+            $('#game-price').val(response.price);
+            $('#game-genre').val(response.genreId);
+            $('#game-publisher').val(response.publisherId);
+            $('#game-platform').val(response.platformId);
+
+            $('#myModal').modal('show');
+            $('#update-button').show();
+            $('#create-button').hide();
+        },
+        error: function (errormessage) {
+        }
+    });
+}
+
+function CreateGame() {
+    var gameObj = {
+        gameName: $('#game-name').val(),
+        gameDescription: $('#game-description').val(),
+        releaseDate: $('#game-release-date').val(),
+        price: $('#game-price').val(),
+        genreId: $('#game-genre').val(),
+        publisherId: $('#game-publisher').val(),
+        platformId: $('#game-platform').val()
+    }
+
+    $.ajax({
+        url: "/Game/AddGame",
+        type: "POST",
+        data: JSON.stringify(gameObj),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            if (response.success == true) {
+
+                console.log(response);
+
+                $('#error-message').text('Game added successfully');
+
+                $('#myModal').modal('hide')
+                $('.modal-backdrop').remove();
+                $('#update-button').hide();
+                $('#create-button').show();
+
+                ReadGames();
+
+            }
+            else {
+                console.log(response.success)
+                $('#error-message').text('Please verify your input');
+            }
+        },
+        error: function (errormessage) {
+            console.log(errormessage.responseText);
+        }
+    })
+}
+
+
+function UpdateGame() {
+    var gameObj = {
+        GameId: $('#game-id').val(),
+        GameName: $('#game-name').val(),
+        GameDescription: $('#game-description').val(),
+        ReleaseDate: $('#game-release-date').val(),
+        Price: $('#game-price').val(),
+        GenreId: $('#game-genre').val(),
+        PublisherId: $('#game-publisher').val(),
+        PlatformId: $('#game-platform').val()
+    }
+
+    console.log(`gameObj: ${gameObj}`);
+
+    $.ajax({
+        url: "/Game/UpdateGame",
+        type: "POST",
+        data: JSON.stringify(gameObj),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+
+            ReadGames();
+
+            $('#myModal').modal('hide');
+            $('.modal-backdrop').remove();
+        },
+        error: function (errormessage) {
+            console.log(errormessage.responseText);
+        }
+    })
+}
+
+function DeleteGame(gameId) {
+    console.log(`gameId: ${gameId}`)
+
+    var ans = confirm('Are you sure you want to delete this game?');
+    if (ans) {
+        $.ajax({
+            url: "/Game/DeleteGame/" + gameId,
+            data: { gameId: gameId },
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+                ReadGames();
+            },
+            error: function (errormessage) {
+                if (errormessage.status == 401)
+                    console.log('You are not authorized!');
+            }
+        });
+    }
+}
+
